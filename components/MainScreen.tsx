becc7fadda02421a4ac29a914b485d51522f0cf9
daniilsmiirnov <dan_smirnov@mail.ru>
2023-12-10 22:43:49 +0300

    search

--- components/MainScreen.tsx
+++ components/MainScreen.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, FlatList } from 'react-native';
+import { View, FlatList, TextInput, StyleSheet } from 'react-native';
 import axios from 'axios';
 import ObjectCard, { ObjectInt } from './ObjectCard';
 import NavigationBar from './NavBar';
@@ -7,6 +7,8 @@ import NetworkInfo from 'react-native-network-info';
 
 const MainScreen: React.FC = () => {
   const [objects, setObjects] = useState<ObjectInt[]>([]);
+  const [filteredObjects, setFilteredObjects] = useState<ObjectInt[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>('');
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,17 +16,29 @@ const MainScreen: React.FC = () => {
         const response = await axios.get<ObjectInt[]>('http://172.20.10.2:8000/object/');
         if (response.status === 200) {
           setObjects(response.data);
+          setFilteredObjects(response.data);
         } else {
           throw new Error('Failed to get data from the server');
         }
       } catch (error) {
         console.error(error);
-
       }
     };
 
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if (searchQuery === '') {
+      setFilteredObjects(objects);
+    } else {
+      const filtered = objects.filter(
+        (object) =>
+          object.Name_Obj.toLowerCase().includes(searchQuery.toLowerCase())
+      );
+      setFilteredObjects(filtered);
+    }
+  }, [searchQuery, objects]);
 
   const renderObjectCard = ({ item }: { item: ObjectInt }) => {
     return (
@@ -36,20 +50,41 @@ const MainScreen: React.FC = () => {
   };
 
   const handleObjectPress = (object: ObjectInt) => {
-
     console.log('Pressed:', object.Name_Obj);
   };
 
   return (
-    <View>
-      <NavigationBar /> 
+    <View style={styles.container}>
+      <NavigationBar />
+      <TextInput
+        style={styles.input}
+        placeholder="Поиск по названию..."
+        onChangeText={(text) => setSearchQuery(text)}
+        value={searchQuery}
+      />
       <FlatList
-        data={objects}
+        data={filteredObjects}
         renderItem={renderObjectCard}
         keyExtractor={(item) => item.ID_Object.toString()}
       />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 0,
+  },
+  input: {
+    height: 40,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginBottom: 10,
+    marginTop:10,
+  },
+});
 
 export default MainScreen;
